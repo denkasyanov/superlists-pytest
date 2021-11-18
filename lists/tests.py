265abfa31b831362ca0de6eced63d4fda37cc094
Den Kasyanov <den@den.ai>
2021-11-18 03:55:43 +0300

    First unit test, urls mapping and a dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,2 +1,7 @@
-def test_bad_maths():
-    assert 1 + 1 == 3
+from django.urls import resolve
+from lists.views import home_page
+
+
+def test_root_url_resolvers_to_home_page_view():
+    found = resolve("/")
+    assert found.func == home_page
