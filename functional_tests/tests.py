08581592193129db10930176973f720fed2e46ef
Den Kasyanov <den@den.ai>
2021-11-20 21:11:12 +0300

    Add first steps of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -120,3 +120,25 @@ def test_multiple_users_can_start_lists_at_different_urls(live_server, browser):
     assert "Buy milk" in page_text
 
     browser.quit()
+
+
+def test_layout_and_styling(live_server, browser):
+    # Edith goes to the home page
+    browser.get(live_server.url)
+    browser.set_window_size(1024, 768)
+
+    # She notices the input box is nicely centered
+    inputbox = browser.find_element(By.ID, "id_new_item")
+    assert inputbox.location["x"] + inputbox.size["width"] / 2 == pytest.approx(
+        512, abs=10
+    )
+
+    # She starts a new list and sees the input is nicely
+    # centered there too
+    inputbox.send_keys("testing")
+    inputbox.send_keys(Keys.ENTER)
+    wait_for_row_in_list_table(browser, "1: testing")
+    inputbox = browser.find_element(By.ID, "id_new_item")
+    assert inputbox.location["x"] + inputbox.size["width"] / 2 == pytest.approx(
+        512, abs=10
+    )
