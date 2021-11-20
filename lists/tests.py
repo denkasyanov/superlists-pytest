675d10b33d285b299672decf6f2b71aea116b40f
Den Kasyanov <den@den.ai>
2021-11-20 19:36:32 +0300

    Associate items with lists

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.urls import resolve
 import pytest
 from pytest_django.asserts import assertContains, assertRedirects, assertTemplateUsed
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 pytestmark = pytest.mark.django_db
@@ -27,8 +27,10 @@ def test_users_list_template(client):
 
 
 def test_displays_all_items(client):
-    Item.objects.create(text="itemey 1")
-    Item.objects.create(text="itemey 2")
+    list_ = List.objects.create()
+
+    Item.objects.create(text="itemey 1", list=list_)
+    Item.objects.create(text="itemey 2", list=list_)
 
     response = client.get("/lists/the-only-list-in-the-world/")
 
@@ -51,23 +53,33 @@ def test_redirects_after_post(client):
     assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 
-# model tests
+# list and item model tests
 
 
 def test_saving_and_retrieveing_items():
+    list_ = List()
+    list_.save()
+
     first_item = Item()
     first_item.text = "The first (ever) list item"
+    first_item.list = list_
     first_item.save()
 
     second_item = Item()
     second_item.text = "Item the second"
+    second_item.list = list_
     second_item.save()
+
+    saved_list = List.objects.first()
+    assert saved_list == list_
 
     saved_items = Item.objects.all()
     assert saved_items.count() == 2
 
     first_saved_item = saved_items[0]
     assert first_saved_item.text == "The first (ever) list item"
+    assert first_saved_item.list == list_
 
     second_saved_item = saved_items[1]
     assert second_saved_item.text == "Item the second"
+    assert second_saved_item.list == list_
