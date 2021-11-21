631a28de4c59c7d3d47fd7cf7a9023404eb60be1
Den Kasyanov <den@den.ai>
2021-11-22 02:15:17 +0300

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -75,38 +75,6 @@ def test_redirects_after_post(client):
     assertRedirects(response, f"/lists/{list_.id}/")
 
 
-# list and item model tests
-
-
-def test_saving_and_retrieveing_items():
-    list_ = List()
-    list_.save()
-
-    first_item = Item()
-    first_item.text = "The first (ever) list item"
-    first_item.list = list_
-    first_item.save()
-
-    second_item = Item()
-    second_item.text = "Item the second"
-    second_item.list = list_
-    second_item.save()
-
-    saved_list = List.objects.first()
-    assert saved_list == list_
-
-    saved_items = Item.objects.all()
-    assert saved_items.count() == 2
-
-    first_saved_item = saved_items[0]
-    assert first_saved_item.text == "The first (ever) list item"
-    assert first_saved_item.list == list_
-
-    second_saved_item = saved_items[1]
-    assert second_saved_item.text == "Item the second"
-    assert second_saved_item.list == list_
-
-
 # new item tests
 
 
