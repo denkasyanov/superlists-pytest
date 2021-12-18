e2a25c27c46f2d86a84f45139f4fce73c83fcd41
Den Kasyanov <den@den.ai>
2021-12-19 01:01:43 +0300

    Remove exploratory test

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -3,11 +3,6 @@ import pytest
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 
 
-def test_form_renders_item_text_input():
-    form = ItemForm()
-    pytest.fail(form.as_p())
-
-
 def test_form_item_input_has_placeholder_and_css_classes():
     form = ItemForm()
     assert 'placeholder="Enter a to-do item"' in form.as_p()
