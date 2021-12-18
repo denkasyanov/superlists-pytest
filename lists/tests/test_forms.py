6f8423e73cc83dcdbfb037b8200cfeef23ecc6a3
Den Kasyanov <den@den.ai>
2021-12-19 00:22:38 +0300

    Add a new form for list items

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,20 @@
+import pytest
+
+from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+
+
+def test_form_renders_item_text_input():
+    form = ItemForm()
+    pytest.fail(form.as_p())
+
+
+def test_form_item_input_has_placeholder_and_css_classes():
+    form = ItemForm()
+    assert 'placeholder="Enter a to-do item"' in form.as_p()
+    assert 'class="form-control input-lg' in form.as_p()
+
+
+def test_form_validation_for_blank_items():
+    form = ItemForm(data={"text": ""})
+    assert not form.is_valid()
+    assert form.errors["text"] == [EMPTY_ITEM_ERROR]
