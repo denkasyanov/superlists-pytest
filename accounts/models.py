ec02f1e2399c3803c7e28e581561db244c64b377
Den Kasyanov <den@den.ai>
2021-12-26 19:03:59 +0300

    Add user and token models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    is_anonymous = False
+    is_authenticated = True
+
+
+class Token(models.Model):
+    uid = models.UUIDField(default=uuid4, primary_key=True)
+    email = models.EmailField()
