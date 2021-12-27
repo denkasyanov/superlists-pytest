2f5a56b3e4670ec7d083b1f908ae0fecf557d670
Den Kasyanov <den@den.ai>
2021-12-28 02:25:10 +0300

    Add url scheme for accounts

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
+
+from accounts import urls as account_urls
 from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
     path("", list_views.home_page, name="home"),
     path("lists/", include(list_urls)),
+    path("accounts/", include(account_urls)),
     # path('admin/', admin.site.urls),
 ]
